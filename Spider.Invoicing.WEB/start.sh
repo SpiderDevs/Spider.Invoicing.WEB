25c40dda42415de5132fda6c4054f1b3bc800417
BigOsDev <bigosdev0@gmail.com>
2018-05-15 23:15:20 +0200

    auto restart docker

--- Spider.Invoicing.WEB/start.sh
+++ Spider.Invoicing.WEB/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker stop spider-invoicing-web || true && docker rm -f spider-invoicing-web || true
-docker run -d --name spider-invoicing-web -p 4200:4200 spider-invoicing-web
+docker run -d --name spider-invoicing-web -p 4200:4200 --restart always spider-invoicing-web
