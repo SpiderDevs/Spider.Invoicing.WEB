d3af02f39952142c3668dfd18649dc93932ed86d
BigOsDev <bigosdev0@gmail.com>
2018-05-16 18:52:55 +0200

    local env

--- Spider.Invoicing.WEB/src/environments/environment.uat.ts
+++ Spider.Invoicing.WEB/src/environments/environment.uat.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  invoicingApi: 'http://vps482108.ovh.net:15030',
-  identityServer: 'http://vps482108.ovh.net:15000',
-  host: 'http://vps482108.ovh.net:4200',
+  invoicingApi: 'http://laptop:15030',
+  identityServer: 'http://laptop:15000',
+  host: 'http://laptop:4200',
 };
