cc8a2427277ec5f160d7d2aaeada14dc6ed0458a
BigOsDev <bigosdev0@gmail.com>
2018-07-18 14:55:16 +0200

    fix port

--- Spider.Invoicing.WEB/src/environments/environment.uat.ts
+++ Spider.Invoicing.WEB/src/environments/environment.uat.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   invoicingApi: 'http://spiderdev.ml:15030',
   identityServer: 'http://spiderdev.ml:15000',
-  host: 'http://spiderdev.ml:4200',
+  host: 'http://spiderdev.ml',
 };
